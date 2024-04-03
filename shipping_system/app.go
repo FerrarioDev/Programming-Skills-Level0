dc7c0a712f76a624a40dcebcc5f2d386d40a38c5
Geronimo Ferrario <ferrario.dev@gmail.com>
2024-04-03 00:23:38 +0000

    fix(login now works and verifyes passwords and if the user tries to login 3 times and fails it will return that the user is blocked)

--- shipping_system/app.go
+++ shipping_system/app.go
@@ -26,11 +26,18 @@ func register(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
 		username := r.FormValue("username")
 		password := r.FormValue("password")
+		// Check if username already exists
+		if _, ok := users[username]; ok {
+			http.Error(w, "Username already exists", http.StatusBadRequest)
+			fmt.Println("Username already exists")
+			return
+		}
 		user.Username = username
 		user.Password = password
 		user.LoginAttempts = 0
 		users[user.Username] = &user
 		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		fmt.Println("User registered successfully", users)
 	}
 	tmpl, err := template.ParseFiles("templates/register.html")
 	if err != nil {
@@ -44,20 +51,24 @@ func login(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
 		username := r.FormValue("username")
 		password := r.FormValue("password")
-		user, ok := users[username]
-		if user.Password != password || !ok {
-		
-			user.LoginAttempts++
-			http.Error(w, "Invalid username or password", http.StatusUnauthorized)
-			return
+		if user, ok := users[username]; ok {
+			if user.Password == password{
+				user.LoginAttempts = 0
+				http.Redirect(w, r, "/", http.StatusSeeOther)
+			} else {
+				user.LoginAttempts++
+				http.Error(w, "Invalid username or password", http.StatusUnauthorized)
+				fmt.Println("Invalid username or password")
+				if user.LoginAttempts > 3 {
+					http.Error(w, "Too many failed login attempts, user is blocked", http.StatusUnauthorized)
+					fmt.Println("User is blocked", user)
+					return
+				}
+				return
+			}	
 		}
-		
-		if user.LoginAttempts > 3 {
-			http.Error(w, "Too many failed login attempts, user is blocked", http.StatusUnauthorized)
-			return
-		}
-
-		http.Redirect(w, r, "/home", http.StatusSeeOther)
+		http.Error(w, "User not found", http.StatusUnauthorized)
+		fmt.Println("User not found")
 	}
 	tmpl, err := template.ParseFiles("templates/login.html")
 	if err != nil {
